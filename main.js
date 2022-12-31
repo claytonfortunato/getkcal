a45a2b428bf3d8b60eea9b37ceae8352059540a0
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-12-30 22:08:30 -0300

    praticando

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#form");
+const btn = document.querySelector("#submit");
+
+btn.addEventListener("submit", (e) => {
+  e.preventDefault;
+  console.log(btn);
+});
